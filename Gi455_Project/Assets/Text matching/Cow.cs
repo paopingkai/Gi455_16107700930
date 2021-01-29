1b1eb4c891740714dd9f36014f53cb476c99f1d2
พีรพัฒน์ แหยมสอาด <peerapat.yeam@bumail.net>
2021-01-29 22:31:56 +0700

    Send Assignment week1

---
+++ Gi455_Project/Assets/Text matching/Cow.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Cow : MonoBehaviour
+{
+    public Text Apple;
+    public InputField Orang;
+
+    public void Mango()
+    {
+        string Kiwi = Orang.text;
+        Apple.text = Kiwi;
+
+        if (Kiwi == "Unity" )
+        {
+            Apple.text = " <color=Blue>[It is a keyword]</color> ]" ;
+        }
+
+        if (Kiwi == "Dota 2")
+        {
+            Apple.text = " <color=Blue>[It is a keyword]</color> ";
+        }
+
+        if (Kiwi == "ResidentEvil")
+        {
+            Apple.text = " <color=Blue>[It is a keyword]</color> ";
+        }
+
+        if (Kiwi == "Pubg")
+        {
+            Apple.text = " <color=Blue>[It is a keyword</color>] ";
+        }
+
+        if (Kiwi == "Valorant")
+        {
+            Apple.text = " <color=Blue>[It is a keyword</color>] ";
+        }
+    }
+
+}
